4e21fcbbe45db37e44563ffca6962b2634785389
Haylie Guzman <hguzma15@stu.southtexascollege.edu>
2024-09-22 15:22:53 -0500

    Added Balance component and integrated budget tracking in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <script setup>
 import Header from './components/Header.vue';
 import AddGrocery from './components/AddGrocery.vue';
+import Balance from './components/Balance.vue';
+
+import { ref, computed } from 'vue';
+const groceries = ref([]);
+const budget = ref(150);
+
+const totalCost = computed(() => {
+  return groceries.value.reduce((acc, item) => {
+    return acc + item.cost;
+  }, 0);
+});
+
+const remainingBudget = computed(() => {
+  return budget.value - totalCost.value;
+});
+
+const handleGrocery = (groceryData) => {
+  groceries.value.push({
+    id: generateID(),
+    name: groceryData.name,
+    cost: groceryData.cost,
+  });
+};
+
+const generateID = () => {
+  return Math.floor(Math.random() * 10000000);
+};
+
 
 </script>
 
 <template>
   <Header></Header>
   <AddGrocery></AddGrocery>
+  <Balance :total="totalCost" :budget="budget" :remaining="remainingBudget" />
+
 </template>
 
---
+++ src/components/Balance.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  },
+  budget: {
+    type: Number,
+    required: true,
+  },
+  remaining: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h4>Your Budget</h4>
+    <h1 id="budget">{{ budget.toFixed(2) }}</h1>
+    <h4>Your Total Expenses</h4>
+    <h1 id="total">{{ total.toFixed(2) }}</h1>
+    <h4>Remaining Budget</h4>
+    <h1 id="remaining">{{ remaining.toFixed(2) }}</h1>
+  </div>
+</template>
